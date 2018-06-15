b114366df752c66a49884b9fe92b19c1f6bb6a0d
dev <38168258+LINCPlatform@users.noreply.github.com>
2018-06-15 03:55:38 +0100

    Min block count

--- db.h
+++ db.h
@@ -16,7 +16,7 @@
 
 static inline int GetRequireHeight(const bool testnet = fTestNet)
 {
-    return testnet ? 0 : 5000;
+    return testnet ? 0 : 30000;
 }
 
 std::string static inline ToString(const CService &ip) {
